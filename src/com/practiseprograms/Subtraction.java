3d70d783e9348efb6778dab75d83010ff243647c
Rupali <rupalimahamuni2596@gmail.com>
2023-01-19 13:41:36 +0530

    Subtraction class added

---
+++ src/com/practiseprograms/Subtraction.java
@@ -0,0 +1,12 @@
+package com.practiseprograms;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		
+		int a = 8;
+		int b = 19;
+		int c = a-b;
+		System.out.println(c);
+	}
+}
